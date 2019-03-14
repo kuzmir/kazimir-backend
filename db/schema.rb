e1898fbe9f5b0ec986486bce5509a3a8e80bd123
Jan Kraus <kraus.jan@gmail.com>
2019-03-14 17:02:18 +0000

    Update dependencies

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20150507214337) do
     t.text     "description",        limit: 65535
     t.string   "image_file_name",    limit: 255
     t.string   "image_content_type", limit: 255
-    t.integer  "image_file_size",    limit: 4
+    t.integer  "image_file_size",    limit: 8
     t.datetime "image_updated_at"
     t.integer  "place_id",           limit: 4
     t.datetime "created_at",                       null: false
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 20150507214337) do
 
   create_table "streets", force: :cascade do |t|
     t.string   "name",       limit: 255,                   null: false
-    t.boolean  "active",     limit: 1,     default: false, null: false
+    t.boolean  "active",                   default: false, null: false
     t.datetime "created_at",                               null: false
     t.datetime "updated_at",                               null: false
     t.text     "path",       limit: 65535
